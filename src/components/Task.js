1b07db494570d268f413a914add28448e1b6b974
Justin <justinhwu95@gmail.com>
2019-06-17 14:03:34 -0400

    Updated Task js

---
+++ src/components/Task.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Task = (props) => {
+  return(
+    <div class="ui form">
+      <div class="fields">
+        <div class="field">
+          <label>Description</label>
+          <input type="text" placeholder="Description">
+        </div>
+        <div class="field">
+          <label>Due Date</label>
+          <input type="text" placeholder="Due Date">
+        </div>
+        <div class="field">
+          <label>Last name</label>
+          <input type="text" placeholder="Last Name">
+        </div>
+      </div>
+    </div>
+
+
+  )
+}
